9ef0bac77cc51e01dcd56ef6d4b024941a0c53f1
Exitroz <ezeobih@gmail.com>
2023-05-29 23:06:52 +0100

    authentication system

--- accounts/views.py
+++ accounts/views.py
@@ -21,18 +21,18 @@ def signup(request):
         email = email.strip().lower()
         if ('@' not in email) or (email[-4:] not in '.com.org.edu.gov.net'):
             messages.error(request, 'Your Email, ' + email + ', Is invalid!')
-            return render(request, 'register.html')
+            return render(request, 'signup.html')
         if User.objects.filter(email=email).exists():
             messages.error(request, 'Your Email, ' + email + ',  Already Exists. Please Try Another Email')
-            return render(request, 'register.html')
+            return render(request, 'signup.html')
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Your Username, ' + username + ', Already Exists. Please Try Another Username')
-            return render(request, 'register.html')
+            return render(request, 'signup.html')
 
 
         if password != password1:
             messages.error(request, "Your passwords Don't match")
-            return render(request, 'register.html')
+            return render(request, 'signup.html')
         User.objects.create_user(email=email, first_name=first_name, last_name=last_name, 
                                     username=username, password=password)
         context = {
@@ -41,8 +41,8 @@ def signup(request):
             'last_name': last_name,
             'username': username
         }
-        return redirect('login')
-    return render(request, 'register.html')
+        return redirect('home')
+    return render(request, 'signup.html')
 
 
 def user_login(request):
@@ -50,25 +50,25 @@ def user_login(request):
         email = request.POST['email']
         password = request.POST['password']
 
-
         email = email.strip().lower()
         if ('@' not in email) or (email[-4:] not in '.com.org.edu.gov.net'):
             messages.error(request, 'Your Email, ' + email +', Is Invalid!')
-            return render(request, 'login.html')
+            return render(request, 'signin.html')
         if not User.objects.filter(email=email).exists():
             messages.error(request, 'This email' + email + ', Does Not exists...')
-            return render(request, 'login.html')
+            return render(request, 'signin.html')
         else:
-            user = authenticate(request, username=email, password=password)
+            user = authenticate(request, email=email, password=password)
             if user is not None:
-                if user.is_active:
-                    login(request, user)
-            return redirect('/')
-    return render(request, 'login.html')
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Username or password does not exist')
+    return render(request, 'signin.html')
 
 
-@login_required
+
 def user_logout(request):
-    logout(request, user)
-    return redirect('login')
+    logout(request)
+    return redirect('home')
                 
